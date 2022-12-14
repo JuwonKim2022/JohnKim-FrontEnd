7cc441c3e6b4e7bbe8b27e8a2307c05f12e7094c
JuwonKim2022 <popolily12@gmail.com>
2022-12-14 17:16:36 +0900

    JS Study 221214 - 2

---
+++ js/ajax.js
@@ -0,0 +1,102 @@
+products.forEach((a,i) => {
+  let 템플릿 = 
+  `<div class="col-sm-4">
+    <img src="https://via.placeholder.com/600" class="w-100">
+    <h5>${products[i].title}</h5>
+    <p>가격 : ${products[i].price}</p>
+  </div>`;
+document.querySelector('.row').insertAdjacentHTML('beforeend', 템플릿);
+});
+
+let count = 0;
+document.querySelector('#more').addEventListener('click', function(e){
+  count++;
+  console.log(count);
+  if(count == 1){
+    정보가져오기(1);
+  }else if(count == 2){
+    정보가져오기(2);
+    document.querySelector('#more').classList.add('hide');
+  }
+});
+
+function 정보가져오기(j) {
+  $.get('https://codingapple1.github.io/js/more'+j+'.json')
+      .done((data) => {
+        console.log(data);
+
+        data.forEach((a,i) => {
+          let 템플릿 = 
+          `<div class="col-sm-4">
+            <img src="https://via.placeholder.com/600" class="w-100">
+            <h5>${data[i].title}</h5>
+            <p>가격 : ${data[i].price}</p>
+          </div>`;
+        document.querySelector('.row').insertAdjacentHTML('beforeend', 템플릿);
+        });
+      });
+};
+
+// 3-10. array 정렬
+document.querySelector('#numArr').addEventListener('click',function() {
+  products.sort(function(a,b){
+    return a.price - b.price
+  });
+  console.log(products);
+
+  document.querySelector('.row').innerHTML = '';
+
+  products.forEach((a,i) => {
+    let 템플릿 = 
+    `<div class="col-sm-4">
+      <img src="https://via.placeholder.com/600" class="w-100">
+      <h5>${products[i].title}</h5>
+      <p>가격 : ${products[i].price}</p>
+    </div>`;
+  document.querySelector('.row').insertAdjacentHTML('beforeend', 템플릿);
+  });
+})
+
+document.querySelector('#textArr').addEventListener('click',function() {
+  products.sort(function(a,b){
+    if(a.title < b.title){
+      return 1;
+    }else if(a.title = b.title){
+      return 0;
+    }else if(a.title > b.title){
+      return -1;
+    } 
+  });
+  console.log(products);
+
+  document.querySelector('.row').innerHTML = '';
+
+  products.forEach((a,i) => {
+    let 템플릿 = 
+    `<div class="col-sm-4">
+      <img src="https://via.placeholder.com/600" class="w-100">
+      <h5>${products[i].title}</h5>
+      <p>가격 : ${products[i].price}</p>
+    </div>`;
+  document.querySelector('.row').insertAdjacentHTML('beforeend', 템플릿);
+  });
+})
+
+document.querySelector('#underSix').addEventListener('click',function() {
+  let usProducts = products.filter(function(a){
+    return a.price <= 60000
+  })
+  console.log(usProducts);
+
+  document.querySelector('.row').innerHTML = '';
+
+  usProducts.forEach((a,i) => {
+    let 템플릿 = 
+    `<div class="col-sm-4">
+      <img src="https://via.placeholder.com/600" class="w-100">
+      <h5>${usProducts[i].title}</h5>
+      <p>가격 : ${usProducts[i].price}</p>
+    </div>`;
+  document.querySelector('.row').insertAdjacentHTML('beforeend', 템플릿);
+  });
+})
