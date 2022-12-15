3492a65620e047999c54448cd58e9a701dac919e
JuwonKim2022 <popolily12@gmail.com>
2022-12-15 09:48:34 +0900

    JS Study 221215

--- JSBasic/js/ajax.js
+++ JSBasic/js/ajax.js
@@ -4,6 +4,7 @@ products.forEach((a,i) => {
     <img src="https://via.placeholder.com/600" class="w-100">
     <h5>${products[i].title}</h5>
     <p>가격 : ${products[i].price}</p>
+    <button class="buy">구매</button>
   </div>`;
 document.querySelector('.row').insertAdjacentHTML('beforeend', 템플릿);
 });
@@ -31,6 +32,7 @@ function 정보가져오기(j) {
             <img src="https://via.placeholder.com/600" class="w-100">
             <h5>${data[i].title}</h5>
             <p>가격 : ${data[i].price}</p>
+            <button class="buy">구매</button>
           </div>`;
         document.querySelector('.row').insertAdjacentHTML('beforeend', 템플릿);
         });
@@ -52,6 +54,7 @@ document.querySelector('#numArr').addEventListener('click',function() {
       <img src="https://via.placeholder.com/600" class="w-100">
       <h5>${products[i].title}</h5>
       <p>가격 : ${products[i].price}</p>
+      <button class="buy">구매</button>
     </div>`;
   document.querySelector('.row').insertAdjacentHTML('beforeend', 템플릿);
   });
@@ -77,6 +80,7 @@ document.querySelector('#textArr').addEventListener('click',function() {
       <img src="https://via.placeholder.com/600" class="w-100">
       <h5>${products[i].title}</h5>
       <p>가격 : ${products[i].price}</p>
+      <button class="buy">구매</button>
     </div>`;
   document.querySelector('.row').insertAdjacentHTML('beforeend', 템플릿);
   });
@@ -96,7 +100,24 @@ document.querySelector('#underSix').addEventListener('click',function() {
       <img src="https://via.placeholder.com/600" class="w-100">
       <h5>${usProducts[i].title}</h5>
       <p>가격 : ${usProducts[i].price}</p>
+      <button class="buy">구매</button>
     </div>`;
   document.querySelector('.row').insertAdjacentHTML('beforeend', 템플릿);
   });
 })
+
+// 3-14. 장바구니
+// document.querySelector('.buy').addEventListener('click',function(e){
+//   let title = e.target.previousElementSibling.previousElementSibling;
+// });
+
+$('.buy').click(function(e){
+  let title = $(e.target).siblings('h5').text();
+  if(localStorage.getItem('cart') != null){
+    let 꺼낸거 = JSON.parse(localStorage.getItem('cart'));
+    꺼낸거.push(title);
+    localStorage.setItem('cart', JSON.stringify(꺼낸거));
+  } else{
+    localStorage.setItem('cart', JSON.stringify([title]));
+  }
+});
